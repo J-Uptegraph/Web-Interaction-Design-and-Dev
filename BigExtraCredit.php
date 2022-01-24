a95752a64ad90501de713b1be66837bb3575a352
Johnathan Uptegraph <96315658+J-Uptegraph@users.noreply.github.com>
2022-01-24 10:56:44 -0500

    Add files via upload

---
+++ BigExtraCredit.php
@@ -0,0 +1,76 @@
+<!-- <!doctype html>
+<html lang="en">
+<head>
+<link rel="icon" type="image/png" href="favicon.ico">
+<meta name="viewport" content="initial-scale = 1,width=device-width"/>
+<meta name="description" content="Johnathan's BigExtraCredit">
+<meta name="author" content="Johnathan Uptegraph">
+<meta charset="utf-8">
+<title>Johnathan's BigExtraCredit</title>
+<style>
+img {
+  margin-left: 5%;
+  margin-right: 5%;
+  width: 90%;
+}
+body {
+  color: #efefef;
+  background-color: #60B2E5;
+  text-shadow: 2px 2px #111111;
+  padding: 1em;
+}
+h1 {
+  margin-top: 1em;
+  text-shadow: 3px 3px #111111;
+  font-size: 2em;
+}
+</style>
+</head>
+<header>
+</header>
+<body>
+<h1>Johnathan's Big Extra Credit</h1>
+<p>
+Here are my responsive design original images in the required size of 1000 x 300.
+</p>
+<div style="margin-bottom:3em;">
+<h2>New York Comic Shop by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/new_york_comic_shop_by_Johnathan_Uptegraph.jpg" alt="New York Comic Shop by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>A New York City Street by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/new_york_city_street_by_Johnathan_Uptegraph.jpg" alt="a shot of the band by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>Saint John's Lagoon by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/saint_john__lagoon_by_Johnathan_Uptegraph.jpg" alt="sunrise by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>high school band concert by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/high_school_band_concert_by_Johnathan_Uptegraph.jpg" alt="high school band concert by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>Jerseys On by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/just_guys_being_dudes_by_Johnathan_Uptegraph.jpg" alt="Guys Being Dudes by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>SalesForce Tower by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/salesforce_tower_by_Johnathan_Uptegraph.jpg" alt="me and my boy by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>A Night Out by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/some_friends_by_Johnathan_Uptegraph" alt="the studio by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>  by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/y_Johnathan_Uptegraph.jpg" alt="by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>  by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/y_Johnathan_Uptegraph.jpg" alt="by Johnathan Uptegraph">
+</div>
+<div style="margin-bottom:3em;">
+<h2>  by Johnathan Uptegraph</h2>
+<img src="BigExtraCreditImages/y_Johnathan_Uptegraph.jpg" alt="by Johnathan Uptegraph">
+</div>
+</html> -->
